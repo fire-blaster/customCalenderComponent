88400a404a0761e6a5b1b6ad5f498dd6d02d07b7
iMac <imac@192.168.1.3>
2023-06-18 20:52:57 +0530

    added date validation

--- App.js
+++ App.js
@@ -12,15 +12,14 @@ const nDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 const App = () => {
   const [activeDate, setActiveDate] = useState(new Date());
+
   const generateMatrix = () => {
     const matrix = [];
     matrix[0] = weekDays.map((day) => day.substring(0, 3));
   
     const year = activeDate.getFullYear();
-    const month = activeDate.getMonth();
-    const today = activeDate.getDate();
-  
-    const firstDay = new Date(year, month, 1).getDay();
+    let month = activeDate.getMonth();
+    let firstDay = new Date(year, month, 1).getDay();
   
     if (firstDay === 0) {
       firstDay = 6;
@@ -38,94 +37,26 @@ const App = () => {
     let rowIndex = 1;
     let colIndex = firstDay;
   
+    for (let i = 1; i < 7; i++) {
+      matrix[i] = new Array(7).fill(-1);
+    }
+  
     while (counter <= maxDays) {
-      if (!matrix[rowIndex]) {
-        matrix[rowIndex] = [];
-      }
-      if (rowIndex === 1 && colIndex < today) {
-        matrix[rowIndex][colIndex] = -1; // Disable dates before today
-      } else {
-        matrix[rowIndex][colIndex] = counter;
-        colIndex++;
-        if (colIndex === 7) {
-          colIndex = 0;
-          rowIndex++;
-        }
-        counter++;
-      }
-    }
+      matrix[rowIndex][colIndex] = counter;
+      counter++;
+      colIndex++;
   
-    if (firstDay !== 0) {
-      for (let i = 0; i < firstDay; i++) {
-        matrix[1].unshift(-1);
-      }
-    }
-  
-    const lastRow = matrix[matrix.length - 1];
-    if (lastRow.length < 7) {
-      const remainingDays = 7 - lastRow.length;
-      for (let i = 0; i < remainingDays; i++) {
-        lastRow.push(-1);
+      if (colIndex === 7) {
+        colIndex = 0;
+        rowIndex++;
       }
     }
   
     return matrix;
   };
   
-  // const generateMatrix = () => {
-  //   const matrix = [];
-  //   matrix[0] = weekDays.map(day => day.substring(0, 3));
-
-  //   const year = activeDate.getFullYear();
-  //   let month = activeDate.getMonth();
-  //   let firstDay = new Date(year, month, 1).getDay();
-
-  //   if (firstDay === 0) {
-  //     firstDay = 6;
-  //   } else {
-  //     firstDay -= 1;
-  //   }
-
-  //   const maxDays = nDays[month];
-
-  //   if (month === 1 && ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)) {
-  //     maxDays += 1;
-  //   }
-
-  //   let counter = 1;
-  //   let rowIndex = 1;
-  //   let colIndex = firstDay;
-
-  //   while (counter <= maxDays) {
-  //     if (!matrix[rowIndex]) {
-  //       matrix[rowIndex] = [];
-  //     }
-  //     matrix[rowIndex][colIndex] = counter;
-  //     colIndex++;
-  //     if (colIndex === 7) {
-  //       colIndex = 0;
-  //       rowIndex++;
-  //     }
-  //     counter++;
-  //   }
-
-  //   if (firstDay !== 0) {
-  //     for (let i = 0; i < firstDay; i++) {
-  //       matrix[1].unshift(-1);
-  //     }
-  //   }
-
-  //   const lastRow = matrix[matrix.length - 1];
-  //   if (lastRow.length < 7) {
-  //     const remainingDays = 7 - lastRow.length;
-  //     for (let i = 0; i < remainingDays; i++) {
-  //       lastRow.push(-1);
-  //     }
-  //   }
-
-  //   return matrix;
-  // };
-
+  
+  
   const matrix = generateMatrix();
 
   const rows = matrix.map((row, rowIndex) => (
@@ -161,9 +92,10 @@ const App = () => {
 
   const _onPress = (item) => {
     if (!isNaN(item) && item !== -1) {
-      const selectedDate = new Date(activeDate.getFullYear(), activeDate.getMonth(), item);
       const currentDate = new Date();
-      if (selectedDate <= currentDate) {
+      const selectedDate = new Date(activeDate.getFullYear(), activeDate.getMonth(), item);
+  
+      if (selectedDate >= currentDate || selectedDate.toDateString() === currentDate.toDateString()) {
         const formattedDate = `${selectedDate.getDate()}/${selectedDate.getMonth() + 1}/${selectedDate.getFullYear()}`;
         Alert.alert("Date is:", formattedDate);
         setActiveDate((prevDate) => {
@@ -172,11 +104,14 @@ const App = () => {
           return newDate;
         });
       } else {
-        Alert.alert("Invalid Date", "You cannot select future dates.");
+        Alert.alert("Invalid Date", "You cannot select past dates.");
       }
     }
   };
   
+
+  
+
   
 
   const changeMonth = (n) => {
